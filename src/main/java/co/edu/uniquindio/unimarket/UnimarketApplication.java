2493bfa1fc8b0340dca662a1d30b4ffe8ed3b5b6
camilo2312 <42982644+camilo2312@users.noreply.github.com>
2023-02-26 19:31:54 -0500

    Archivos iniciales del proyecto

---
+++ src/main/java/co/edu/uniquindio/unimarket/UnimarketApplication.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.unimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class UnimarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnimarketApplication.class, args);
+    }
+}
