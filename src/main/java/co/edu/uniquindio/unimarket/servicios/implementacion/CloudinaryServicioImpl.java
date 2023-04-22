ba269cc32afc4a04061de39d7f26053e33a16143
DanielTusarma <118632587+DanielTusarma@users.noreply.github.com>
2023-04-22 18:17:44 -0500

    Se agrega servicio de cloudinary para subir las imagenes

---
+++ src/main/java/co/edu/uniquindio/unimarket/servicios/implementacion/CloudinaryServicioImpl.java
@@ -0,0 +1,41 @@
+package co.edu.uniquindio.unimarket.servicios.implementacion;
+import co.edu.uniquindio.unimarket.servicios.interfaces.CloudinaryServicio;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+@Service
+public class CloudinaryServicioImpl implements CloudinaryServicio {
+    private final Cloudinary cloudinary;
+    public CloudinaryServicioImpl(){
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", "dajsnzdgb");
+        config.put("api_key", "raxX6SwPsoJnWCmK3_Irpr2nCZI");
+        config.put("api_secret", "raxX6SwPsoJnWCmK3_Irpr2nCZI");
+        cloudinary = new Cloudinary(config);
+    }
+    @Override
+    public Map subirImagen(File file, String carpeta) throws Exception{
+        return cloudinary.uploader().upload(file, ObjectUtils.asMap("folder",
+                String.format("uniquindio/unimarket/%s", carpeta)));
+    }
+    @Override
+    public Map eliminarImagen(String idImagen) throws Exception{
+        return cloudinary.uploader().destroy(idImagen, ObjectUtils.emptyMap());
+    }
+    @Override
+    public File convertir(MultipartFile imagen) throws IOException {
+        File file = File.createTempFile(imagen.getOriginalFilename(), null);
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(imagen.getBytes());
+        fos.close();
+        return file;
+    }
+}
+
